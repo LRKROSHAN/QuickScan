b5ffa0c170994d80a6b86c27aa10959c73b97a64
Roshankumar Loganathan <82709418+LRKROSHAN@users.noreply.github.com>
2023-12-05 00:33:01 -0500

    Create device_maker_a.py

---
+++ device_maker_a.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+
+LOGIN_FORM = """
+<html>
+    <body>
+        <form action="" method="post">
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
+"""
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == "admin" and password == "pass":
+            return "Logged in successfully!"
+        else:
+            return "Login Failed!"
+    return render_template_string(LOGIN_FORM)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
