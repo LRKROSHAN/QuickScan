27b5bdb3fe779cab1425c290c4a33ce0858bbc02
Roshankumar Loganathan <82709418+LRKROSHAN@users.noreply.github.com>
2023-12-06 03:05:34 -0500

    Update quickscan.py

--- quickscan.py
+++ quickscan.py
@@ -1,72 +1,49 @@
-#!/usr/bin/env python3
-
-import subprocess
+import nmap
 import json
-import re
 import requests
 
-
-CREDENTIALS_FILE = 'credentials.json'
-
-def load_credentials():
-    with open(CREDENTIALS_FILE, 'r') as file:
-        return json.load(file)
-
-def scan_network(network_range):
+# Function to attempt login with given credentials
+def attempt_login(url, username, password):
     try:
-        result = subprocess.run(["nmap", "-sn", network_range], capture_output=True, text=True, check=True)
-        output = result.stdout
-        ip_addresses = re.findall(r'(\d+\.\d+\.\d+\.\d+)', output)
-        return ip_addresses
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while scanning the network: {e}")
-        return []
+        response = requests.post(url, data={'username': username, 'password': password})
+        if response.status_code == 200 and "Logged in successfully" in response.text:
+            return True
+        return False
+    except requests.RequestException:
+        return False
 
-def identify_iot_devices(devices):
-    iot_devices = []
-    for device in devices:
-        try:
-            result = subprocess.run(["nmap", "-p", "80,443", device], capture_output=True, text=True, check=True)
-            output = result.stdout
-            if "80/tcp open" in output or "443/tcp open" in output:
-                iot_devices.append(device)
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred while scanning device {device}: {e}")
-    return iot_devices
+# Load default credentials
+with open('credentials.json') as f:
+    default_credentials = json.load(f)
 
-def check_default_credentials(iot_devices, credentials):
-    vulnerable_devices = []
-    for device in iot_devices:
-        for cred in credentials:
-            try:
-                url = f"http://{device}/login"
-                response = requests.post(url, data={'username': cred['default_username'], 'password': cred['default_password']})
-                if response.ok:
-                    vulnerable_devices.append({'device': device, 'credentials': cred})
-                    break
-            except requests.RequestException as e:
-                print(f"An error occurred while trying to log into {device}: {e}")
-    return vulnerable_devices
+# Initialize Nmap Scanner
+nm = nmap.PortScanner()
 
-def generate_report(vulnerable_devices):
-    if not vulnerable_devices:
-        print("No vulnerable devices found.")
-        return
-    print("Vulnerable Devices Found:")
-    for device in vulnerable_devices:
-        print(f"Device: {device['device']}, Credentials: {device['credentials']['default_username']}/{device['credentials']['default_password']}")
+# Scan the local network for open ports 5000 and 5001
+nm.scan(hosts='10.0.2.0/24', arguments='-p 5000,5001')
 
-    with open('vulnerability_report.txt', 'w') as file:
-        for device in vulnerable_devices:
-            file.write(f"Device: {device['device']}, Credentials: {device['credentials']['default_username']}/{device['credentials']['default_password']}\n")
+# Open a file to write the vulnerability report
+with open('vulnerability_report.txt', 'w') as report_file:
+    vulnerabilities_found = False
+    for host in nm.all_hosts():
+        for port in ['5000', '5001']:
+            if nm[host].has_tcp(int(port)) and nm[host]['tcp'][int(port)]['state'] == 'open':
+                url = f'http://{host}:{port}'
+                for cred in default_credentials:
+                    if attempt_login(url, cred['default_username'], cred['default_password']):
+                        report = f"Vulnerabilities Found. Device: {host}, Credentials: {cred['default_username']}/{cred['default_password']}\n"
+                        report_file.write(report)
+                        print(report.strip())  # Print to terminal
+                        vulnerabilities_found = True
+                        break
 
-def main():
-    network_range = '192.168.0.0/24'
-    credentials = load_credentials()
-    all_devices = scan_network(network_range)
-    iot_devices = identify_iot_devices(all_devices)
-    vulnerable_devices = check_default_credentials(iot_devices, credentials)
-    generate_report(vulnerable_devices)
+    if not vulnerabilities_found:
+        report_file.write("No vulnerabilities found.\n")
+        print("No vulnerabilities found.")  # Print to terminal
 
-if __name__ == "__main__":
-    main()
+    report_file.write("\n")
+
+if __name__ == '__main__':
+    print("QuickScan Complete")
+    with open('vulnerability_report.txt', 'a') as report_file:
+        report_file.write("End Of Report\n")
